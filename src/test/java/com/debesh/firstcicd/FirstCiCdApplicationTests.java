81c73d389d25cee161e3ecee628e04d44f488ae4
student <dnayak382@gmail.com>
2021-02-27 16:34:57 +0000

    initial commit

---
+++ src/test/java/com/debesh/firstcicd/FirstCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.debesh.firstcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstCiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
